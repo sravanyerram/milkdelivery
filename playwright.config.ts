c6d5a417e165f4176429ce22cc895b0b4c70cc79
Sravan <sravan.yerram1988@gmail.com>
2026-02-27 10:56:40 +0530

    ci: inject cross-env for Next.js webServer in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'npm run start',
+      command: 'npx cross-env NEXT_PUBLIC_USE_EMULATOR="true" NEXT_PUBLIC_ADMIN_EMAIL="sravan.yerram1988@gmail.com" NEXT_PUBLIC_FIREBASE_API_KEY="dummy" NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="dummy" NEXT_PUBLIC_FIREBASE_PROJECT_ID="demo-milk" npm run start',
       port: 3000,
       reuseExistingServer: !process.env.CI,
       timeout: 120 * 1000,
